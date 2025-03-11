b9f957d95e2ce2ad49c77910fa54d2ec7e8b89c8
Artem Kapustin <103292137+artyomkap@users.noreply.github.com>
2025-03-11 12:11:56 +0200

    Update config.py

--- nft_bot/config.py
+++ nft_bot/config.py
@@ -1,7 +1,7 @@
 TOKEN = '' #BOT TOKEN
 ADMIN_IDS = []
 SQLALCHEMY_URL = 'sqlite+aiosqlite:///nft.db'
-TEXT_CHANNEL_ID = '-1002212671394'
+TEXT_CHANNEL_ID = ''
 AGREEMENT_URL = 'https://support.opensea.io' # for messages and keyboard url
 SUPPORT_URL = 'https://support.opensea.io/en/' #for keyboard for support team tg
 MAIN_BOT_API_URL = '' #NGROK OR XTUNNEL or WEBHOOK
